219a2334b07d8bab1d0522d9f214a292d0e2f92e
Andrew Boyer <asboyer@gmail.com>
2021-05-14 11:17:39 -0400

    restructuiring game

--- main.py
+++ main.py
@@ -12,6 +12,14 @@ def fr(name):
     file = open(f"descriptions/{name}.txt", "r")
     return file.read()
 
+def choose(level, choice):
+    delay_print(fr(f'choice{str(choice)}_{str(level)}'))
+    print(f)
+
+    return get_num(fr(f'description{str(choice)}_{str(level)}') + " ", start=0, finish=1, integer=True)
+
+check_alive():
+
 # read a file of highscores
 # if no highscores, make new file
 highscore = 0
@@ -20,12 +28,28 @@ played = 0
 
 while playing:
     # initializing variables
+    choice = ""
     score = 0
     alive = True
     win = False
-    level = 0
+    level = 1
 
     clear()
     starter(played)
 
+    while alive and not win:
+        choice = choose(level)
+        level += 1
+        check_alive()
+
+    if win:
+        # winner
+    else:
+        # death
     # choice
+
+
+
+
+
+
