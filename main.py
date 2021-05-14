7f456bc8cfae3ccc9f644b689d823ffa2b96d567
boyer <asboyer@gmail.com>
2021-05-14 18:33:26 -0400

    added more functions

--- main.py
+++ main.py
@@ -1,48 +1,89 @@
 from boyer import *
+from art import text2art
+from os import path
+
+speed = 0
 
 def starter(played):
     if played == 0:
         # set the scene
-        delay_print(fr('starter'))
+        delay_print(fr('intro'), speed)
         print("\n")
         
     else:
         pass
-        # you know the drill, run it back
 
 def fr(name):
-    file = open(f"descriptions/{name}.txt", "r")
+    file = open(f"text/{name}.txt", "r")
     return file.read()
 
 def choose(level, choice):
-    delay_print(fr(f'description{str(choice)}_{str(level)}'))
+    delay_print(fr(f'descriptions/description{str(choice)}_{str(level)}'), speed)
 
-    return get_num(fr(f'choice{str(choice)}_{str(level)}'), start=0, finish=1, integer=True)
+    return get_num(fr(f'choices/choice{str(choice)}_{str(level)}'), start=0, finish=1, integer=True)
 
 def check_alive(level, choice):
-    if level == 1 and choice == 1:
-        delay_print(fr(f'death{str(choice)}_{str(level)}'))
+    death_file = f"text/deaths/death{str(choice)}_{str(level)}"
+
+    if path.exists(death_file):
+        delay_print(fr(death_file), speed)
         return False
     else:
         return True
 
+def check_win(level, choice):
+    win_file = f"text/wins/win{str(choice)}_{str(level)}"
 
-# read a file of highscores
-# if no highscores, make new file
+    if path.exists(win_file):
+        delay_print(fr(win_file), speed)
+        return True
+    else:
+        return False
+
+def play_again():
+    while True:
+        choice = input("Play again? ")
+        if choice.startswith('y'):
+            return True
+        elif choice.startswith('n'):
+            return False
+        else:
+            print('Please enter a yes or no!')
+
+def intro(played):
+    clear()
+    starter(played)
+    delay_print("Welcome to...", speed)
+    print(text2art("Boyer's\nadventure\ngame"))
+    input("\nPress enter to begin\n")
+    clear()
+
+def scorecard(score, highscore, level, played, highlevel):
+    print(f'Level reached: {str(level)}')
+    print(f'Highest level: {str(highlevel)}')
+    print(f'Games played: {str(played)}')
+    print(f'Score: {str(score)}')
+    print(f'Highscore: {str(highscore)}')
+
+def credits():
+    delay_print(fr('credits'), speed)
+
+# TO DO: read a file of highscores
+#       if no highscores, make new file
 highscore = 0
+highlevel = 0
 playing = True
 played = 0
 
 while playing:
-    # initializing variables
+
     choice = ""
     score = 0
     alive = True
     win = False
     level = 1
 
-    clear()
-    starter(played)
+    intro(played)
 
     while alive and not win:
         choice = choose(level, choice)
@@ -51,19 +92,28 @@ while playing:
             score += 1.5 * level
             level += 1
 
+    played += 1
+
     if win:
-        delay_print("YOU WIN!")
-        # winner
+        delay_print("\nYOU WIN!", speed)
+        credits()
+
     else:
-        delay_print("GAME OVER")
-        # death
+        delay_print("\nGAME OVER", speed)
+
     if score > highscore:
         highscore = score
         print("NEW HIGH SCORE!")
         # enter initials
 
-    # play again?
-    break
+    if level > highlevel:
+        highlevel = level
+        print("NEW HIGHEST LEVEL")
+
+    scorecard(score, highscore, level, played, highlevel)
+
+    if not play_again():
+        break
 
 
 
